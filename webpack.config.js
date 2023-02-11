ee746faea761ed4860ea5e9c8e895b07ba22273b
Frankma0117 <franmacagu@gmail.com>
2023-02-10 22:30:44 -0500

    Correción del Plugin Clean

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const htmlPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const CleanWebpackPLugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -56,7 +56,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename : '[name].css'
     }),
-    new CleanWebpackPLugin(),
+    new CleanWebpackPlugin(),
   ],
   optimization : {
     minimize : true,
